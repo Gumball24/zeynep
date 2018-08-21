d5ec6b8981341e99bb58ec69192dcf498b2f339b
Himmet Karahan Buhan <me@karahanbuhan.com>
2018-08-21 18:58:49 +0300

    Add response and better regex, we need elegant way of doing this though. And a configuration.

--- modules/hello/module.py
+++ modules/hello/module.py
@@ -1,3 +1,4 @@
+import random
 import re
 
 import discord
@@ -13,8 +14,10 @@ class HelloModule(Module):
     def init(self):
         self.logger.info("Module enabled.")
 
+    responses = ["merhaba", "as", "selam", "aleyküm selam"]
+
     async def on_message(self, message: discord.Message):
-        if re.match(
-                "((kızlar|beyler|zeynep) )\?(sa|selamun aleyküm|merhaba|selam|hi|hello|naber|)( (kızlar|beyler|zeynep))\?",
-                message.content):
-            await self.client.send_message(message.channel, "as")
+        if re.fullmatch(
+                "((k+ı+z+l+a+r+|b+e+y+l+e+r+|z+e+y+n+e+p+) )?(s+a+|s+e+l+a+m+(ü+|u+)n+ a+l+e+y+k+(ü+|u+)m+|m+e+r+h+a+b+a+|s+e+l+a+m+|h+i+|h+e+l+l+o+|n+a+b+e+r+)( (k+ı+z+l+a+r+|b+e+y+l+e+r+|z+e+y+n+e+p+))?(!+|\.+)?",
+                message.content, re.IGNORECASE):
+            await self.client.send_message(message.channel, random.choice(self.responses))
